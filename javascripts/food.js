b115b68722707c0fd2f4503a6bf289e98252b580
Chandler Morrison <cwm795@gmail.com>
2018-01-09 14:11:48 -0600

    'MVP' of dog food portion'

--- javascripts/food.js
+++ javascripts/food.js
@@ -2,6 +2,7 @@
 console.log("Hey there");
 
 const $ = require('jquery');
+const _StartCase = require("lodash.startcase");
 
 // ajax call
 $.ajax({
@@ -12,9 +13,9 @@ console.log(dogData.dog_brands);
 let $dogFood = $("#dogFood");
 dogData.dog_brands.forEach( (brand, index) =>{
         // console.log(`brand${index}`);
-        $("#dogFood").append(`<h3>${brand.name}</h3>`);
+        $("#dogFood").append(`<h3>${_StartCase(brand.name)}</h3>`);
         brand.types.forEach( (brandType) => {
-            $("#dogFood").append(`<h4>${brandType.type}</h4>`);
+            $("#dogFood").append(`<h4>${_StartCase(brandType.type)}</h4>`);
                 brandType.volumes.forEach( (vol) => {
                     $dogFood.append(`<h5> Size: ${vol.name} Price: $${vol.price}</h5>`);
              });
